b9b2532c63b13bcddd7df286609cd4fd59b66e5e
DanielHuey <adhueywilan@gmail.com>
2022-09-23 23:58:50 +0300

    edited request by adding a try-catch block

--- Request.java
+++ Request.java
@@ -1,21 +1,34 @@
 import java.io.*;
+import java.util.Scanner;
 
 public class Request {
     public static void requested() throws Exception {
-        System.out.println("Request sent, \n Response may take a while \n You may log in later and the response will be available");
-        FileWriter output = new FileWriter("Request.txt");
-        output.write("Request");
-
-        int i;
-        FileReader input = new FileReader("Request.txt");
-        while ((i = input.read()) != -1) {
-            System.out.print((char)i);
-
+        FileWriter output = new FileWriter("Request.txt",false);
+        
+        try {
+            FileReader input = new FileReader("Request.txt");
+            Scanner scan = new Scanner(input);
+            while (scan.hasNextLine()) {
+                String line = scan.nextLine();//looking for a line like response=Name:dan,Points:345,Return Buyers:12,....
+                if (line.contains("response")) {
+                    String[] content = line.split("=");//splits at =
+                    String[] response = content[1].split(",");//gets part after the '=' and splits using commas
+                    for (String string : response) {
+                        System.out.println(string);//prints each onto a new line
+                    }
+                }
+            }
+            System.out.println("New request being made to server");
+        } catch (Exception e) {
+            //this part only runs the first time a request is made
+            System.out.println("Request sent, \n Response may take a while \n You may check later and the response will be available");
+        } finally {
+            //this part runs all the time
+            output.write("Request");
+            output.close();
         }
-        System.out.println("\n");
-
-        input.close();
-
+        
+        
     }
 }
 
