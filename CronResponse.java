23d73d8650a7f20bed84e7bf3f45086b333ec913
Ghasasira <gasasiraj013@outlook.com>
2022-09-23 23:16:10 +0300

    just chill tweaking

--- CronResponse.java
+++ CronResponse.java
@@ -3,7 +3,7 @@ import java.util.concurrent.TimeUnit;
 
 public class CronResponse{
     public static void get() throws Exception {
-        System.out.println("Process running, wait about 160 seconds.");
+        System.out.println("Process running, wait about 2 minutes.");
         TimeUnit.SECONDS.sleep(10);
         FileReader r = new FileReader("anka.txt");
         //code here to read from anka.txt/anka.json after cron job
--- Request.java
+++ Request.java
@@ -1,8 +1,11 @@
-import java.io.FileReader;
+import java.io.*;
 
 public class Request {
     public static void requested() throws Exception {
         System.out.println("Request sent, \n Response may take a while \n You may log in later and the response will be available");
+        FileWriter output = new FileWriter("Request.txt");
+        output.write("Request");
+
         int i;
         FileReader input = new FileReader("Request.txt");
         while ((i = input.read()) != -1) {
