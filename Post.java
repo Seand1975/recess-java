d11d1852fa936b65a5cbf7d4941022771a96fd87
DanielHuey <adhueywilan@gmail.com>
2022-09-24 06:36:38 +0300

    post

--- Post.java
+++ Post.java
@@ -1,13 +1,14 @@
 import java.io.*;
 
 public class Post implements Serializable {
-
+    public static boolean posted = false;
     public static void postProduct(String name, String desc, String price) throws Exception {
-        if (name.equals(Session.sessItem)) {
+        if (name.equals(Session.sessItem)&& !posted ) {
             String data = "post: "+Session.sessName+"," + name + "," + desc + "," + price + "\n";
             FileWriter out = new FileWriter("anka.txt", true);
             out.write(data);
             out.close();
-        }else CronResponse.error("That is not the product you registered.");
+            posted = true;
+        }else CronResponse.error("You have already posted. Use Qty_update to update quantities.");
     }
 }
